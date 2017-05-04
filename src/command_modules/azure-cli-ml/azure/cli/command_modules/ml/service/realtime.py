52144f877fcf20a788b1dae1ec814e39dc08c31e
chris11313 <pirillo.chris@gmail.com>
2017-05-03 21:24:23 -0700

    # This is a combination of 2 commits. # This is the 1st commit message:
    # This is a combination of 2 commits.
    # This is the 1st commit message:
    
    Rebasing
    
    # This is the commit message #2:
    
    Adds command to deploy batch FE. Fixes target port in batch service.
    
    # This is the commit message #2:
    
    Adds command to deploy batch FE. Fixes target port in batch service.

--- src/command_modules/azure-cli-ml/azure/cli/command_modules/ml/service/realtime.py
+++ src/command_modules/azure-cli-ml/azure/cli/command_modules/ml/service/realtime.py
@@ -1176,7 +1176,8 @@ def realtime_service_run_kubernetes(context, service_name, input_data, verbose):
             print('If you just created this service, it may not be available yet. Please try again in a few minutes.')
         elif '%MatchError' in content:
             print('Unable to find service with name {}'.format(service_name))
-        print(content)
+        else:
+            print(content)
         return
 
     try:
